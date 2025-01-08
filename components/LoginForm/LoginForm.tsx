07c5dafa020bb91f053060bde8f86f120d946de8
Dominic <dominicezeifejafor@gmail.com>
2025-01-08 13:33:47 +0100

    Creating components

--- components/LoginForm/LoginForm.tsx
+++ components/LoginForm/LoginForm.tsx
@@ -1,12 +1,20 @@
-import { CardWrapper } from '@/shared'
+import { Button, CardWrapper, InputField } from '@/shared'
 import React from 'react'
 
 const LoginForm = () => {
   return (
     <CardWrapper headerLabel='Welcome back' backButtonLabel={`Don't have an account`}
-    backButtonHref='/auth/register' showSocial
+      backButtonHref='/auth/register' showSocial
     >
-        Login Form
+      Login Form
+      <form className='flex flex-col gap-2'>
+        <InputField name='name' label='Name' placeholder='Enter your ' />
+        <InputField name='Email' label='email' placeholder='Enter your ' />
+        <InputField label='' placeholder='Enter your ' />
+        <Button type='submit'>
+          Submit
+        </Button>
+      </form>
     </CardWrapper>
   )
 }
